a13d2afa512e20a742fd3662a83693d127991a57
Yan Velasco <yancarlosduartevelasco@gmail.com>
2025-02-09 14:35:37 -0300

    feat: update styles and dependencies, remove unused CSS file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import { FaBeer } from "react-icons/fa"
 
 function App() {
 
   return (
     <div >
        <div>
-          <h1 className='text-3xl font-bold'>React App</h1>
+          <h1 className='flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold'>Welcome<FaBeer/></h1>
        </div>
     </div>
   )
