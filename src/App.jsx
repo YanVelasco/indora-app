753689d926ed8cc9911af35daeb1a88c1bc1b684
Yan Velasco <yancarlosduartevelasco@gmail.com>
2025-02-09 14:11:57 -0300

    feat: project created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div >
+       <div>
+          <h1 className='text-3xl font-bold'>React App</h1>
+       </div>
+    </div>
+  )
+}
+
+export default App
