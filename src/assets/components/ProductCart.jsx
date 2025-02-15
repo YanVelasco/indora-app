d782e3a68c3451186f852b6a8810e2952e092f02
Yan Velasco <yancarlosduartevelasco@gmail.com>
2025-02-15 14:59:16 -0300

    feat: enhance ProductCart component with animations and prop validation, update Products component to use ProductCard

--- src/assets/components/ProductCart.jsx
+++ src/assets/components/ProductCart.jsx
@@ -1,9 +1,76 @@
-const ProductCart = () => {
+import { motion } from 'framer-motion';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { FaShoppingCart } from 'react-icons/fa';
+
+const containerVariants = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } }
+};
+
+const ProductCart = ({ product }) => {
+    const [openProductViewModal, setOpenProductViewModal] = useState(false);
+    const btnLoader = false;
+    const [selectedProductViewModal, setSelectedProductViewModal] = useState("");
+    const isAvailable = product.quantity > 0;
+
+    const handleProductViewModal = (product) => {
+        setSelectedProductViewModal(product);
+        setOpenProductViewModal(true);
+    }
+
     return (
-        <div>
-            <h2>Product Cart</h2>
+        <div className="h-full">
+            <motion.div
+                key={product.id}
+                className="bg-white shadow-lg rounded-lg overflow-hidden transition-shadow-lg hover:shadow-xl h-full flex flex-col"
+                variants={containerVariants}
+                initial="hidden"
+                animate="visible"
+            >
+                <div onClick={() => handleProductViewModal(product)} className='w-full overflow-hidden aspect-[3/2]' >
+                    <img className="cursor-pointer w-full transition-transform duration-300 transform hover:scale-105" src={product.image} alt={product.name} />
+                </div>
+                <div className="p-6 flex flex-col flex-grow">
+                    <h2 onClick={() => {product}} className="text-xl font-semibold text-gray-800">{product.name}</h2>
+                    <div className='min-h-20 max-h-20 overflow-hidden'>
+                        <p className="text-gray-600 text-sm mt-2 flex-grow">{product.description}</p>
+                    </div>
+                    <div className="mt-4 flex items-center justify-between">
+                        <div className="flex items-center justify-between">
+                            {product.price ? (
+                                <div className='flex flex-col gap-0.5'>
+                                    <span className="text-lg font-semibold line-through text-gray-700">${product.price.toFixed(2)}</span>
+                                    <span className="text-xl font-bold text-red-600">${product.specialPrice.toFixed(2)}</span>
+                                </div>
+                            ) : (
+                                <span className="text-xl font-semibold text-gray-800">${product.price.toFixed(2)}</span>
+                            )}
+                        </div>
+                        <button className={`px-4 py-2 text-white font-semibold rounded-lg ${isAvailable ? "bg-green-500 hover:bg-green-600 cursor-pointer" : "bg-red-400 opacity-75 cursor-not-allowed"}`} disabled={!isAvailable || btnLoader}>
+                            <div className='flex items-center justify-center'>
+                                <FaShoppingCart className="inline-block mr-2" />
+                                {isAvailable ? "Add to Cart" : "Out of Stock"}
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </motion.div>
         </div>
     );
 }
+
+ProductCart.propTypes = {
+    product: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        quantity: PropTypes.number.isRequired,
+        price: PropTypes.number.isRequired,
+        discount: PropTypes.number.isRequired,
+        specialPrice: PropTypes.number.isRequired,
+    }).isRequired,
+};
 
 export default ProductCart;
--- src/assets/components/Products.jsx
+++ src/assets/components/Products.jsx
@@ -1,16 +1,16 @@
-import { FaExclamationTriangle } from "react-icons/fa";
-import ProductCart from "./ProductCart";
+import { FaExclamationTriangle } from 'react-icons/fa';
+import ProductCard from './ProductCart';
 
 const Products = () => {
     const loading = false;
-    const errorMessage = "Error fetching products";
+    const errorMessage = "";
     const products = [
         {
             id: "123e4567-e89b-12d3-a456-426614174000",
             name: "Iphone Xs max",
             image: "https://placehold.co/600x400",
             description: "Experience the latest in mobile technology with advanced cameras, powerful processing, and an all-day battery.",
-            quantity: 0,
+            quantity: 1,
             price: 1450.0,
             discount: 10.0,
             specialPrice: 1305.0,
@@ -26,6 +26,9 @@ const Products = () => {
             specialPrice: 2040.0,
         }
     ];
+
+    
+
     return (
         <div className="lg:px-14 sm:px-8 px-4 py-14 2xl:w-[90%] 2xl:mx-auto">
             {loading && (
@@ -34,17 +37,17 @@ const Products = () => {
                 </div>
             )}
             {!loading && errorMessage && (
-                <div className="flex items-center align- justify-center h-screen">
-                    <FaExclamationTriangle className="text-slate-800 text-3xl mr-2" />
-                    <span className="text-slate-800 text-lg font-medium">{errorMessage}</span>
+                <div className="flex items-center justify-center h-screen">
+                    <FaExclamationTriangle className="text-slate-800 text-4xl mr-2" />
+                    <span className='text-slate-800 text-lg font-medium'>{errorMessage}</span>
                 </div>
             )}
             {!loading && !errorMessage && (
                 <div className="min-h-[700px]">
                     <div className="pb-6 pt-14 grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8">
-                        {
-                            products?.map((product, i) => <ProductCart key={i}  {...product} />   
-                       )}
+                        {products.map((product) => 
+                            <ProductCard key={product.id} product={product} />
+                        )}
                     </div>
                 </div>
             )}
