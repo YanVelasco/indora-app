539fc2a7d650c1e19aa90a6a3ac65cc377249a63
Yan Velasco <yancarlosduartevelasco@gmail.com>
2025-03-03 11:19:41 -0300

    feat: implement routing by adding Home component and updating App structure to use React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Home } from "./components/home/Home";
 import Products from "./components/products/Products";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <div >
-      <Products />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
