7773c7afc78a75a21c5ba40a1886aed3ae9a4404
Romulo de Sousa Amaro <romulo@capemisa.com.br>
2025-06-27 12:35:17 -0300

    Atualizacao do App removendo linhas comentadas desnecessarias

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import { Home } from "./components/home/Home";
 import Products from "./components/products/Products";
-// import { NavBar } from "./components/shared/NavBar";
-// import { About } from "./components/about/About";
-// import { Contact } from "./components/contact/Contact";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// import Home from "./components/home/Home";
-// import Products from "./components/products/Products";
-
 import NavBar from "./components/shared/NavBar";
 import Footer from "./components/shared/Footer";
 import About from "./components/about/About";
-// import ProductCart from "./components/shared/ProductCart";
 import Cart from "./components/shared/Cart";
 import VLibras from "./components/vlibras/VLibras";
 import SignIn from "./pages/SignIn";
@@ -33,7 +24,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          {/* <Route path="/shared" element={<ProductCart />} /> */}
           <Route path="/shared" element={<Cart />} />
           <Route path="/products" element={<Products />} />
           <Route path="/signin" element={<SignIn />} />
